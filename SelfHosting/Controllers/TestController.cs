21c87631d330450548f2eff6beefcb6fed7faaf5
Zepsen <Zepsen@DESKTOP-5RNEI0T>
2016-06-22 10:17:01 +0300

    Add project files.

---
+++ SelfHosting/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SelfHosting.Controller
+{
+    public class TestController : ApiController
+    {       
+        // GET api/demo 
+        public string Get()
+        {
+            return "Tessst";
+        }
+
+        // GET api/demo/5 
+        public string Get(int id)
+        {
+            return "Hello, World!";
+        }
+
+        // POST api/demo 
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/demo/5 
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/demo/5 
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SelfHosting/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.Owin.Hosting;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SelfHosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ip = GetLocalIPAddress();
+            var port = 50000;
+                        
+            using (WebApp.Start<Startup>($"http://{ip}:{port}"))
+            {
+                Console.WriteLine($"Web Server is running on {ip}:{ port}.");
+                Console.WriteLine("Press any key to quit.");
+                Console.ReadLine();
+            }
+
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+    }
+}
